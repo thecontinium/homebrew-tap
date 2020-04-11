307b45fa125a683164f1ce94205a1bb4e1399ad4
the continium <thecontinium@outlook.com>
2020-04-11 16:07:13 +0100

    Update zprint.rb

--- zprint.rb
+++ zprint.rb
@@ -2,10 +2,10 @@ class Zprint < Formula
   desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
   @@alias_version = "0.5.4"
-  @@alias_name = "zprintm-#{@alias_version}"
+  @@alias_name = "zprintm-#{@@alias_version}"
   url "https://github.com/kkinnear/zprint/releases/download/#{@@alias_version}/#{@@alias_name}"
   sha256 "281fb1749bbb1eb2102d5c8bb0db013fbbea0d5b3eb2889f6cd4da06d1e588a5"
-  version '0.0.20'
+  version '0.0.21'
   
   def install
     bin.install "#{@@alias_name}"
