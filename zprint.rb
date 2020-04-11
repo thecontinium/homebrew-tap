2cc89746633a01b399e863a0cdfde4b7db168498
the continium <thecontinium@outlook.com>
2020-04-11 14:30:38 +0100

    Update zprint.rb

--- zprint.rb
+++ zprint.rb
@@ -2,7 +2,8 @@ class Zprint < Formula
   desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
   url "https://github.com/kkinnear/zprint/releases/download/0.5.4/zprint-filter-0.5.4"
-  version '0.0.6'
+  sha256 "625857e8711b72ab71e875bb48b00650c0e31f223860d15b089074e3497925f8"
+  version '0.0.7'
   
   def install
     bin.install 'zprint-filter-0.5.4'
