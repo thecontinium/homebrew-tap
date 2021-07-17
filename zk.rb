9da3fcf2339b9b8f912c230a21eb5452cdfb17ab
thecontinium <thecontinium@outlook.com>
2021-07-17 20:29:24 +0100

    add zk

---
+++ zk.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Zk < Formula
+  desc "A plain text note-taking assistant"
+  homepage ""
+  url "https://github.com/mickael-menu/zk/releases/download/v0.6.0/zk-v0.6.0-macos-arm64.zip"
+  sha256 "0e709a11af8d8365d78c011a3f8f5e66ff1595763175a7434117421aebe70d9b"
+  license "GPL-3.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test zk`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
