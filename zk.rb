2435a2ada54679d0badcca149d070e6e42c24cdf
thecontinium <thecontinium@outlook.com>
2021-07-17 20:41:16 +0100

    added version

--- zk.rb
+++ zk.rb
@@ -7,6 +7,7 @@ class Zk < Formula
   url "https://github.com/mickael-menu/zk/releases/download/v0.6.0/zk-v0.6.0-macos-arm64.zip"
   sha256 "0e709a11af8d8365d78c011a3f8f5e66ff1595763175a7434117421aebe70d9b"
   license "GPL-3.0"
+  version '0.0.03'
 
   depends_on "go"
 
