8a5f4c889a3c2e4c78dc60caed9193c7f69f1a5e
thecontinium <thecontinium@outlook.com>
2021-07-17 20:47:13 +0100

    sha

--- zk.rb
+++ zk.rb
@@ -5,9 +5,9 @@ class Zk < Formula
   desc "A plain text note-taking assistant"
   homepage ""
   url "https://github.com/mickael-menu/zk/releases/download/v0.6.0/zk-v0.6.0-macos-x86_64.zip"
-  sha256 "0e709a11af8d8365d78c011a3f8f5e66ff1595763175a7434117421aebe70d9b"
+  sha256 "7f312567a62cf722f038e313e61f0092593468311120327407cd207206bad670"
   license "GPL-3.0"
-  version '0.0.04'
+  version '0.0.05'
 
   depends_on "go"
 
