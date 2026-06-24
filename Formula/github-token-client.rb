cb956997a6748d1d587c3205c6b82209829b09e5
the continium <thecontinium@outlook.com>
2026-06-24 13:49:14 +1000

    Bump version from 1.0.0 to 1.0.1

--- Formula/github-token-client.rb
+++ Formula/github-token-client.rb
@@ -2,7 +2,7 @@ class GithubTokenClient < Formula
   desc "Stateless execution interceptor script for GitHub CLI tools"
   homepage "https://github.com/thecontinium/homebrew-tap"
   url "https://github.com/thecontinium/github-token-client/archive/refs/tags/v1.0.0.tar.gz"
-  version "1.0.0"
+  version "1.0.1"
   sha256 "f6fe2680aa6efe9d31e2b758883c0077922e5907e9c67749c71d912e630ec16f"
   license "MIT"
 
